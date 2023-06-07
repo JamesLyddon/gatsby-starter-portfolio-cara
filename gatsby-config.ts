d32cc4631c2557e177168ec648498d037c3150ef
JamesLyddon <jameslyddon@gmail.com>
2023-06-07 12:57:55 +0100

    feat: updated config

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -8,11 +8,19 @@ const config: GatsbyConfig = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-cara/gatsby-config.mjs
-    siteTitle: `Cara`,
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
-    siteHeadline: `Cara - Gatsby Theme from @lekoarts`,
+    // siteTitle: `Cara`,
+    // siteTitleAlt: `James Lyddon Portfolio`,
+    // siteHeadline: `James Lyddon Portfolio`,
+    // siteUrl: `https://cara.lekoarts.de`,
+    // siteDescription: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+    // siteImage: `/banner.jpg`,
+    // siteLanguage: `en`,
+    // author: `@lekoarts_de`,
+    siteTitle: `James Lyddon Portfolio`,
+    siteTitleAlt: `James Lyddon Portfolio`,
+    siteHeadline: `James Lyddon Portfolio`,
     siteUrl: `https://cara.lekoarts.de`,
-    siteDescription: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+    siteDescription: `James Lyddon's font-end developer`,
     siteImage: `/banner.jpg`,
     siteLanguage: `en`,
     author: `@lekoarts_de`,
